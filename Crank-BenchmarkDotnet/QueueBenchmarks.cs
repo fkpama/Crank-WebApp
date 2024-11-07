92141107e15c5ef6aa29539132d19184def5b84f
KPAMA Frederic <ct6625@engie.com>
2024-11-07 15:53:56 +0100

    ok

---
+++ Crank-BenchmarkDotnet/QueueBenchmarks.cs
@@ -0,0 +1,28 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Crank_BenchmarkDotnet;
+
+public class QueueBenchmarks
+{
+	[Params(10, 100, 1000, 10000)]
+
+	public int ArraySize { get; set; }
+
+	[Benchmark(Baseline = true)]
+	public void First_Benchmark()
+	{
+		for (var i = 0; i < ArraySize; i++)
+		{
+			Guid.NewGuid();
+		}
+	}
+
+	[Benchmark]
+	public void Second_Benchmark()
+	{
+		for (var i = 0; i < ArraySize; i++)
+		{
+			Guid.Parse(Guid.NewGuid().ToString());
+		}
+	}
+}
